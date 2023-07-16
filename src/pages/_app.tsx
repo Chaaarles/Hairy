8e39ee357a659ac1c9d917e09894eefc93cc7d91
Petter Charles Redfern <petter.charles@redfern.no>
2023-07-16 12:35:02 +0200

    Fix clerk signin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
-import { type AppType } from "next/app";
-import { api } from "~/utils/api";
+import {type AppType} from "next/app";
+import {api} from "~/utils/api";
 import "~/styles/globals.css";
-import { ClerkProvider } from "@clerk/nextjs/dist/types/components.server";
+import {ClerkProvider} from "@clerk/nextjs";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return (
-    <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />;
-    </ClerkProvider>)
+
+const MyApp: AppType = ({Component, pageProps}) => {
+    return (
+        <ClerkProvider {...pageProps}>
+            <Component {...pageProps} />;
+        </ClerkProvider>
+    )
 };
 
 export default api.withTRPC(MyApp);
