e0fee7f834f5877323dd798a8043a942547015f3
Petter Charles Redfern <petter.charles@redfern.no>
2023-07-16 13:08:41 +0200

    Add goalsRouter

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
-import { exampleRouter } from "~/server/api/routers/example";
 import { createTRPCRouter } from "~/server/api/trpc";
+import { goalsRouter } from "~/server/api/routers/goals";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +7,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  goals: goalsRouter,
 });
 
 // export type definition of API
---
+++ src/server/api/routers/goals.ts
@@ -0,0 +1,7 @@
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const goalsRouter = createTRPCRouter({
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.goal.findMany();
+  }),
+});
